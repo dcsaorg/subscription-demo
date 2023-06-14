be1d2827b6a8a2f5b52a2c61d53f9984e0cd2cd7
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2023-06-14 11:51:42 +0200

    init commit for producer core

---
+++ producer-core/src/main/java/org/dcsa/producercore/ProducerCoreApplication.java
@@ -0,0 +1,14 @@
+package org.dcsa.producercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProducerCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProducerCoreApplication.class, args);
+  }
+
+}
---
+++ producer-core/src/test/java/org/dcsa/producercore/ProducerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dcsa.producercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
