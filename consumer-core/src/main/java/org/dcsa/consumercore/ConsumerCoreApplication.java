bc2577b3d2b30a51bbfc248890ac2d006448237d
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2023-06-15 15:55:21 +0200

    added init commit for consumer core

---
+++ consumer-core/src/main/java/org/dcsa/consumercore/ConsumerCoreApplication.java
@@ -0,0 +1,12 @@
+package org.dcsa.consumercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerCoreApplication.class, args);
+  }
+}
---
+++ consumer-core/src/test/java/org/dcsa/consumercore/ConsumerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dcsa.consumercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
