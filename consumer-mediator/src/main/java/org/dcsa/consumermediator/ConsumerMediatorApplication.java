882ced912e52b299774f41f01c05ab876751d8f2
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2023-06-15 15:47:55 +0200

    added init consumer mediator

---
+++ consumer-mediator/src/main/java/org/dcsa/consumermediator/ConsumerMediatorApplication.java
@@ -0,0 +1,12 @@
+package org.dcsa.consumermediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMediatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMediatorApplication.class, args);
+  }
+}
---
+++ consumer-mediator/src/test/java/org/dcsa/consumermediator/ConsumerMediatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dcsa.consumermediator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerMediatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
